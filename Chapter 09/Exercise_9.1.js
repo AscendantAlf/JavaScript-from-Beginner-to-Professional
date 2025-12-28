f1bd073c995822713bacc64e151f93a33f6ce36d
AscendantAlf <haveloftherock@protonmail.com>
2025-12-28 13:16:18 -0600

    completed chapter 9 exercises. DOM basics mostly

--- Chapter 09/Exercise_9.1.js
+++ Chapter 09/Exercise_9.1.js
@@ -1,1 +1,27 @@
 console.dir(window)
+
+​/* part of the exercise was to find the document object of my browser's window object
+(BOM - browswer object model). 
+
+For reference, I inspected the newTab home page in the firefox browser
+
+After entering console.dir(window) in the console tab, I found
+the document object under <default.properties> which was listed near the end of the window's
+sub objects. The document object looked like below 
+
+Document: function Document()
+​​​
+length: 0
+​​​
+name: "Document"
+​​​
+parseHTMLUnsafe: function parseHTMLUnsafe()
+​​​
+prototype: DocumentPrototype { getElementsByTagName: getElementsByTagName(), getElementsByTagNameNS: getElementsByTagNameNS(), getElementsByClassName: getElementsByClassName(), … }
+​​​
+<prototype>: function Node() 
+
+*/
+
+/* likewise I was asked to find the screen subobject. It was also under <default.properties>
+but it was just another function. I got screen details with console.dir(window.screen) */
--- Chapter 09/Exercise_9.2.js
+++ Chapter 09/Exercise_9.2.js
@@ -1,2 +1,8 @@
 console.log(window.location.protocol);
+
+// https:
+
 console.log(window.location.href);
+
+// https://www.geeksforgeeks.org/html/browser-object-model/
+// was the website I tried it on
